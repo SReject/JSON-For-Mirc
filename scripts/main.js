b1331d4486c78ce6fe4498ccc602530e8241166d
SReject <sreject@comcast.net>
2016-01-27 18:05:09 -0500

    Refactored for templating

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
 (function($){
-    /*
-    work out page resizing
-    */
+    function resize() {
+        var viewSize = $(window).height(),
+            contSize = $('.content').height() + 250;
+            
+        if (viewSize > contSize) {
+            contSize = viewSize;
+        }
+        $('.wrapcontent').height(contSize);
+    }
+    
+
+    $(window).load(function () {
+       $(window).resize(resize);
+       resize();
+    });
 }(jQuery));
